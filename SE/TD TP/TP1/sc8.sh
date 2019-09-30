7d797c6e67e5e95f90c6925ad2178abf073b3dd4
Alexis JACOB <11709412@crit-xubuntu>
2019-09-30 16:57:13 +0200

    Correction du script de copie des fichiers

--- SE/TD TP/TP1/sc8.sh
+++ SE/TD TP/TP1/sc8.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
-
 echo "Saisir le nom du repertoire source:"
 read src
-
 if [ -d $src ]; then
-  echo "Saisir le nom du repertoire destination:"
-  read dest
-
-  echo $src
-  echo $dest
-
-  if [ -d $dest ]; then
-    if [ -x $dest ]; then
-      chmod u+w $dest
+    echo "Saisir le nom du repertoire destination:"
+    read dest
+    if [ -d $dest ]; then
+        if [ -x $dest ]; then
+            chmod u+w $dest
+        fi
+        filelist=`ls $dest`
+        for file in `ls $src` ; do
+            peutcopier=0
+            for f in $filelist ; do
+                if [ $file == $f ]; then
+                    echo "un fichier $file existe déjà dans $dest"
+                    if [ $file -ot $f1 ]; then
+                        echo "et il est plus recent"
+                        peutcopier=1
+                    fi
+                    else
+                    peutcopier=0
+                fi
+            done
+        if [ $peutcopier -eq 0 ]; then
+            cp -v $src/$file $dest >> log.txt
+        fi
+        done
+        chmod u-w $dest
+    else echo "Erreur: le nom ne correspond pas à un repertoire"
     fi
-    filelist=`ls $dest`
-    for file in `ls $src` ; do
-      if [ -f $file ]; then
-        flag=0
-        for f in $filelist ; do
-        if [ $file == $f ]; then
-        echo "un fichier $file existe déjà dans $dest"
-        flag=1
-      fi
-      cp $src/$file $dest
-    done
-  fi
-  done
-  chmod u-w $dest
-  else echo "Erreur: le nom ne correspond pas à un repertoire"
-  fi
-else echo "Erreur: le nom ne correspond pas à un repertoire"
+else echo "Erreur: le nom ne correspond pas à un repertoire"
 fi
