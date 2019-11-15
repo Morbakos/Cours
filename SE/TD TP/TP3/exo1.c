b7fcea7d2065ebf96d5ce34b0bf9cfbdf7950ac2
Morbakos <alexisjacob21@gmail.com>
2019-11-15 01:59:00 -0800

    Ajout cours PHP & SE

--- SE/TD TP/TP3/exo1.c
+++ SE/TD TP/TP3/exo1.c
@@ -4,7 +4,16 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+static int i = 0;
+
+void handler(int sig){
+    i++;
+    fprintf(stderr, "Signal [type: %d] was catch %d time(s)\n", sig, i);
+}
+
 void main () {
+
+    signal(SIGHUP, handler);
     printf("Processus %d en attente de signaux...\n", getpid());
     while (1) {
    	 sleep(5);
